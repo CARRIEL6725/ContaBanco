0153da628caca2d0b088c2022d3b48267703f937
CARRIEL6725 <rafael.carriel21@gmail.com>
2025-04-21 17:25:09 -0300

    Adição de novas informações

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+   public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o primeiro número:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Digite o segundo número:");
+        int num2 = scanner.nextInt();
+
+        int resultado = num1 + num2;
+
+        System.out.println("A soma é: " + resultado);
+
+        scanner.close();
     }
 }
